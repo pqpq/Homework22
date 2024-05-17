bf74912e193b9a7c8427da3ac5673a19218e09b3
mrj <m@badcrumble.net>
2024-05-17 21:15:51 +0100

    Got a simple expression working

--- hw22.cpp
+++ hw22.cpp
@@ -25,11 +25,26 @@ void test(const string& a, const char* b)
 
 string infix(string_view rpn)
 {
-    return "3 - 4";
+    auto a = rpn.find_first_not_of("0123456789");
+    auto b = rpn.find_first_not_of("0123456789", a + 1);
+    auto c = rpn.find_first_not_of(" ", b);
+    if (a == string_view::npos || b == string_view::npos || c == string_view::npos)
+        return "???"; // {};
+
+    const auto first  = rpn.substr(0, a);
+    a++;
+    const auto second = rpn.substr(a, b - a);
+    const auto third  = rpn.substr(c);
+    return string(first) + " " + string(third) + " " + string(second);
 }
 
 int main()
 {
-    test(infix("34-"), "3 - 4");
+    // basics, and make sure we dont care about whitespace round operator
+    test(infix("3 4-"), "3 - 4");
+    test(infix("78 52*"), "78 * 52");
+    test(infix("7 8 *"), "7 * 8");
+    test(infix("7 8*"), "7 * 8");
+    test(infix("7777 8  *"), "7777 * 8");
     return 0;
 }
