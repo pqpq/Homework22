a9bbca861b9dd5ed30bcc8699737488fd1c3aaef
mrj <m@badcrumble.net>
2024-05-18 18:06:14 +0100

    Fix recursion and add brackets
    Put brackets round each expression so everything is unambiguous

--- hw22.cpp
+++ hw22.cpp
@@ -46,32 +46,32 @@ bool is_operator(string_view s)
 
 deque<string_view> tokenise(string_view s)
 {
-    cout << "tokenise(" << s << ") :\n";
+    //cout << "tokenise(" << s << ") :\n";
 
     deque<string_view> tokens;
 
     while (!s.empty())
     {
-        cout << "  '" << s << "' >>> ";
+        //cout << "  '" << s << "' >>> ";
         while (!s.empty() && isspace(s[0])) s.remove_prefix(1);
-        cout << " '" << s << "' -> ";
+        //cout << " '" << s << "' -> ";
 
         if (is_number(s))
         {
             auto end = s.find_first_not_of(digits);
             tokens.push_front(s.substr(0, end));
             s.remove_prefix(end);
-            cout << "dig '" << tokens.back() << "'\n";
+            //cout << "dig '" << tokens.front() << "'\n";
         }
         else if (is_operator(s))
         {
             tokens.push_front(s.substr(0, 1));
             s.remove_prefix(1);
-            cout << "opr '" << tokens.back() << "'\n";
+            //cout << "opr '" << tokens.front() << "'\n";
         }
         else
         {
-            cout << " eh? '" << s[0] << "'\n";
+            //cout << " eh? '" << s[0] << "'\n";
             return { "???" };   // we've encountered something illegal
         }
     }
@@ -79,8 +79,9 @@ deque<string_view> tokenise(string_view s)
     return tokens;
 }
 
-string recurse(deque<string_view> d, size_t n)
+string recurse(deque<string_view> d, size_t& n)
 {
+    cout << "\nrecurse(" << n << ") : ";
     string s = string(" ") + string{d[n]} + " ";
     n++;
     if (n == d.size())
@@ -88,49 +89,43 @@ string recurse(deque<string_view> d, size_t n)
     if (is_number(d[n]))
     {
         s.append(d[n]);
+        n++;
     }
     else
     {
         const auto expression = recurse(d, n);
-        s = expression + s;
+        s.append("(" + expression + ")");
     }
-    n++;
     if (n == d.size())
         return s;
     if (is_number(d[n]))
     {
         s = string(d[n]) + s;
+        n++;
     }
     else
     {
         const auto expression = recurse(d, n);
-        s = expression + s;
+        s = "(" + expression + ")" + s;
     }
 
+    cout << '"' << s << "\"\n";
     return s;
 }
 
 string infix(string_view rpn)
 {
+    cout << "\nInput: \"" << rpn << "\"\n";
     const auto tokens = tokenise(rpn);
     {
         string s;
         for (const auto& t : tokens) s.append(string{t}), s.append(", ");
         cout << "Tokens: " << s << '\n';
     }
-    return recurse(tokens, 0);
-/*
-    if (tokens.size() > 1)
-    {
-        return string(tokens[2]) + " " + string(tokens[0]) + " " + string(tokens[1]);
-    }
-    if (tokens.empty())
-        return "@@@"; // {};
-
-    string s;
-    for (const auto& t : tokens) s.append(string{t}), s.append(", ");
-    return s;
-*/
+    size_t n{0};
+    const auto result = recurse(tokens, n);
+    cout << "Infix: " << result << '\n';
+    return result; //recurse(tokens, 0);
 }
 
 int main()
@@ -143,6 +138,12 @@ int main()
     test(infix("7777 8  *"), "7777 * 8");
 
     // next level
-    test(infix("3 4 - 5 +"), "3 - 4 + 5");
+    test(infix("3 4 - 5 +"), "(3 - 4) + 5");
+
+    // harder
+    test(infix("4  8  +  1  3  +  /"), "(4 + 8) / (1 + 3)");
+    test(infix("28  6  2  4  *  +  /"), "28 / (6 + (2 * 4))");
+    test(infix("4 2 5 * + 1 3 2 * + /"), "(4 + (2 * 5)) / (1 + (3 * 2))");
+
     return 0;
 }
